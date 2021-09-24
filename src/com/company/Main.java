f6438097f53fa03cbb45579db8c8344d07448e13
OliverAndreasen <oliv2058@stud.kea.dk>
2021-09-24 12:26:25 +0200

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -21,24 +22,43 @@ public class Main {
          */
 
         Room[] rooms = new Room[9];
-        rooms[0] = new Room(1,false,true,true,false);
+        rooms[0] = new Room(0,false,true,true,false);
+        rooms[1] = new Room(1, false, true, false, true);
+        rooms[2] = new Room(2, false, false, true, true);
+        rooms[3] = new Room(3, true, false, true, false);
+        rooms[4] = new Room(4, false, false, true, false);
+        rooms[5] = new Room(5, true, false, true, false);
+        rooms[6] = new Room(6, true, true, false, false);
+        rooms[7] = new Room(7, true, true, false, true);
+        rooms[8] = new Room(8, true, false, false, true);
+
+        int currentRoom = rooms[0].whichRoom();
 
 
 
-
-        int currentRoom = room1.whichRoom();
         Scanner sc = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Velkommen til Adventure-Spillet!");
         System.out.println("Du skal vælge en vej at gå");
         System.out.println("Du kan vælge ");
         System.out.println("Du kan skrive 'go north', 'go east', 'go south' eller 'go west");
         System.out.println("Hvilken vej vil du gå");
-        String direction = sc.nextLine();  // Read user input
-
-        System.out.println(room1.direction(direction));
+        //String direction = sc.nextLine();  // Read user input
 
 
+            for (int i = 0; i < rooms.length; i++) {
+                if (currentRoom == i) {
+                    System.out.println("DU ER I RUM " + (rooms[i].whichRoom() + 1));
+                    System.out.println("Hvilken vej vil du gå");
+                    String direction = sc.nextLine();  // Read user input
 
+                   if(rooms[i].direction(direction)) {
+
+
+                   }
+
+
+                }
+            }
 
     }
 }
