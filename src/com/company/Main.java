9a1c82379fd20def7be7a6883d1605e2080e9470
OliverAndreasen <oliv2058@stud.kea.dk>
2021-09-24 11:52:56 +0200

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,49 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //     public void Room(int id, boolean north, boolean east, boolean south, boolean west)
+        Room room1 = new Room(1,false,true,true,false);
+        Room room2 = new Room(2, false, true, false, true);
+        Room room3 = new Room(3, false, false, true, true);
+        Room room4 = new Room(4, true, false, true, false);
+        Room room5 = new Room(5, false, false, true, false);
+        Room room6 = new Room(6, true, false, true, false);
+        Room room7 = new Room(7, true, true, false, false);
+        Room room8 = new Room(8, true, true, false, true);
+        Room room9 = new Room(9, true, false, false, true);
+
+
+
+
+
+
+
+        int currentRoom = room1.whichRoom();
+
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Velkommen til Adventure-Spillet!");
+        System.out.println("Du skal vælge en vej at gå");
+        System.out.println("Du kan vælge ");
+        System.out.println("Du kan skrive 'go north', 'go east', 'go south' eller 'go west");
+        System.out.println("Hvilken vej vil du gå");
+        String direction = sc.nextLine();  // Read user input
+
+        if ( &&  == 1)
+        {
+            room = 2;
+            System.out.println("Going east");
+            System.out.println("You are now in room " + room);
+        }
+
+
+
+
+
     }
 }
 
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -2,4 +2,55 @@ package com.company;
 
 public class Room {
 
+    private int currentRoom;
+    private boolean north;
+    private boolean east;
+    private boolean south;
+    private boolean west;
+
+
+    public Room(int id, boolean north, boolean east, boolean south, boolean west) {
+        this.currentRoom = id;
+        this.north = north;
+        this.east = east;
+        this.south = south;
+        this.west = west;
+
+    }
+
+    public int whichRoom() {
+        return currentRoom;
+    }
+
+    public boolean direction() {
+
+        boolean result = false;
+
+        if (north == true) {
+            result = true;
+        }
+        else if (north != true) {
+            result = false;
+        }
+        if (east == true) {
+            result = true;
+        }
+        else if (east != true) {
+            result = false;
+        }
+        if (south == true) {
+            result = true;
+        }
+        else if (south != true) {
+            result = false;
+        }
+        if (west == true) {
+            result = true;
+        }
+        else if (west != true) {
+            result = false;
+        }
+
+        return result;
+    }
 }
