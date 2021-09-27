87cad7d6359d7fe780d866fbdbe108817ecfa023
OliverAndreasen <oliv2058@stud.kea.dk>
2021-09-27 09:45:21 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
         // creates array to all the rooms.
         Room[] rooms = new Room[9];
 
+        System.out.println("Test");
+
 
         //Room (int id, boolean north, boolean east, boolean south, boolean west)
         // TODO: Change room ids to 1,2,3,4,5,6,7,8,9
