e88db299cbb648751b05ba6483d624f8965cbc28
OliverAndreasen <oliv2058@stud.kea.dk>
2021-09-30 16:19:22 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -58,14 +58,6 @@ public class Main {
             } else {
                 System.out.println(letter);
                 System.out.println("You cant go that way, try again!");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
-                System.out.println("LOL");
             }
         }
     }
