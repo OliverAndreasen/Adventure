5d57531bbdd2aebc5fd3f0b9f0eb90b4ee27c5cb
OliverAndreasen <oliv2058@stud.kea.dk>
2021-10-11 00:44:16 +0200

    attack logic removed

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -99,64 +99,19 @@ public class Game {
                 case "attack":
                     parser.passEnemyInput(input);
                     System.out.println(parser.attack());
-                    /*
-                    Enemy enemy;
-                    if (player.checkIfEquipped()) {
-                        if (enemyName == null) {
-                            enemyName = currentRoom.closestEnemy(currentRoom);
-                            enemy = currentRoom.findEnemy(enemyName, currentRoom);
-                        }
-                        enemy = currentRoom.findEnemy(enemyName, currentRoom);
-                        if (enemy != null) {
-                            if (enemy.getIsAlive()) {
-                                Item item = player.findItemInventory(player.getEquippedWeapon());
-                                Weapon weapon = player.isWeapon(item);
-                                int damage = weapon.getDamage();
-                                player.attack(enemy);
-                                int enemyHealth = enemy.getCurrentHealth();
-                                enemy.setCurrentHealth(enemyHealth - damage);
-                                System.out.println("you attacked " + enemyName + "\nYou dealt " + damage + " damage");
-                                System.out.println(enemyName + " current health is now " + enemy.getCurrentHealth());
-                                Weapon enemyWeapon = enemy.getWeapon();
-                                if (enemy.died()) {
-                                    currentRoom.setRoomItem(enemyWeapon);
-                                    System.out.println(enemyName + " died and dropped " + enemyWeapon.getName());
-                                    currentRoom.removeEnemy(enemy);
-                                } else if (enemyWeapon.ammoLeft() == 0) {
-                                    System.out.println("The enemy tried to attack but has no ammo left");
-                                    System.out.println("Your current health is now " + player.getCurrentHealth());
-                                } else {
-                                    enemy.attack();
-                                    int enemyDamage = enemyWeapon.getDamage();
-                                    playerHealth = player.getCurrentHealth() - enemyDamage;
-                                    player.setCurrentHealth(playerHealth);
-                                    System.out.println(enemyName + " attacked you with " + enemyWeapon.getName() + " and dealt " + enemyDamage + " damage");
-                                    System.out.println("Your current health is now " + player.getCurrentHealth());
-                                    if (enemy.died()) {
-                                        currentRoom.setRoomItem(enemyWeapon);
-                                        System.out.println(enemyName + " died and dropped " + enemyWeapon.getName());
-                                        currentRoom.removeEnemy(enemy);
-                                    }
-                                }
-                            }
-                        } else {
-                            System.out.println("you swung " + player.getEquippedWeapon() + " but hit nothing");
-                        }
-                    } else {
-                        System.out.println("You have to equip a weapon to attack");
-                    }
-
-                     */
                     break;
+
                 case "unequip":
                     player.unEquipWeapon();
                     break;
 
                 case "enemies":
                     System.out.println(currentRoom.getAllEnemies());
+                    break;
 
                 default:
                     System.out.println("invalid command");
+                    break;
             }
         }
     }
